98b4c53e4e9a4477231f50acd6c927ffa9ef0fb6
shanhong cheng <shanhong.cheng@utah.edu>
2016-12-01 13:24:17 -0700

    initial commit

---
+++ app/src/main/java/com/example/shanhong/hellochartdemo/ChartActivitiy.java
@@ -0,0 +1,39 @@
+package com.example.shanhong.hellochartdemo;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import lecho.lib.hellocharts.model.Line;
+import lecho.lib.hellocharts.model.LineChartData;
+import lecho.lib.hellocharts.model.PointValue;
+import lecho.lib.hellocharts.view.LineChartView;
+
+public class ChartActivitiy extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chart_activitiy);
+
+        List<PointValue> values = new ArrayList<PointValue>();
+        values.add(new PointValue(0, 2));
+        values.add(new PointValue(1, 4));
+        values.add(new PointValue(2, 3));
+        values.add(new PointValue(3, 4));
+
+        //In most cased you can call data model methods in builder-pattern-like manner.
+        Line line = new Line(values).setColor(Color.BLUE).setCubic(true);
+        List<Line> lines = new ArrayList<Line>();
+        lines.add(line);
+
+        LineChartData data = new LineChartData();
+        data.setLines(lines);
+
+        LineChartView chart = (LineChartView) findViewById(R.id.chart);
+        chart.setLineChartData(data);
+    }
+}
---
+++ app/src/main/java/com/example/shanhong/hellochartdemo/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.shanhong.hellochartdemo;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void show(View view) {
+        Intent chartIntent = new Intent(this, ChartActivitiy.class);
+        startActivity(chartIntent);
+    }
+}
